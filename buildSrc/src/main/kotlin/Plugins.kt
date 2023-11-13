ce90210cfcd88658e846f6ec65ee3c8003ced305
Andrey9595 <anb95.95@mail.ru>
2023-11-13 18:38:31 +0300

    config auth created

--- buildSrc/src/main/kotlin/Plugins.kt
+++ buildSrc/src/main/kotlin/Plugins.kt
@@ -2,4 +2,6 @@ object Plugins {
     const val APPLICATION = "com.android.application"
 
     const val ANDROID = "org.jetbrains.kotlin.android"
+
+    const val androidLibrary = "com.android.library"
 }
