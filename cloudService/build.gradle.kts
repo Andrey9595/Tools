9d4ef04d410d7566255f6416daf1c5db3cc9490d
Andrey9595 <anb95.95@mail.ru>
2024-01-16 20:46:59 +0300

    added cloud service module

---
+++ cloudService/build.gradle.kts
@@ -0,0 +1,28 @@
+plugins {
+    id("base-lib-convention")
+}
+
+android {
+    namespace = "com.example.cloudservice"
+
+    defaultConfig {
+        testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
+        consumerProguardFiles("consumer-rules.pro")
+    }
+
+    buildTypes {
+        release {
+            isMinifyEnabled = false
+            proguardFiles(
+                getDefaultProguardFile("proguard-android-optimize.txt"),
+                "proguard-rules.pro"
+            )
+        }
+    }
+}
+
+dependencies {
+    implementation(libs.androidx.ktx)
+    implementation(platform(libs.firebase.bom))
+    implementation(libs.firebase.database)
+}
---
+++ cloudService/src/main/java/com/example/cloudservice/CloudServiceImpl.kt
@@ -0,0 +1,91 @@
+package com.example.cloudservice
+
+import com.google.firebase.database.DataSnapshot
+import com.google.firebase.database.DatabaseError
+import com.google.firebase.database.DatabaseReference
+import com.google.firebase.database.ValueEventListener
+import kotlinx.coroutines.channels.awaitClose
+import kotlinx.coroutines.flow.callbackFlow
+import kotlin.coroutines.resume
+import kotlin.coroutines.resumeWithException
+import kotlin.coroutines.suspendCoroutine
+
+class CloudServiceImpl(provideDatabase: ProvideDatabase) : CloudService {
+    
+    private val reference = provideDatabase.database()
+    
+    override suspend fun postWithIdGenerating(obj: Any?, vararg children: String): String {
+        return suspendCoroutine { cont ->
+            makeReference(*children).push().setValue(obj) { error, ref ->
+                error?.let { cont.resumeWithException(IllegalStateException(error.message)) }
+                    ?: cont.resume(ref.key!!)
+            }
+        }
+    }
+    
+    override suspend fun <T : Any> getDataSnapshot(
+        clazz: Class<T>,
+        vararg children: String
+    ): T = suspendCoroutine { cont ->
+        makeReference(*children).get().addOnSuccessListener { snapshot ->
+            snapshot.getValue(clazz)?.let { cont.resume(it) }
+                ?: cont.resumeWithException(IllegalStateException("Problem with deserialization"))
+        }.addOnFailureListener {
+            cont.resumeWithException(IllegalStateException("Problem with data"))
+        }.addOnCanceledListener {
+            cont.resumeWithException(IllegalStateException("Canceled"))
+        }
+    }
+    
+    override fun post(obj: Any?, vararg children: String) {
+        makeReference(*children).setValue(obj)
+    }
+    
+    override fun <T : DatabaseListItem> addItemToList(item: T, vararg children: String) {
+        val ref = makeReference(*children)
+        ref.updateChildren(mapOf(item.id to item))
+    }
+    
+    override fun <T : DatabaseListItem> subscribe(clazz: Class<T>, vararg children: String) =
+        callbackFlow {
+            val listener = object : ValueEventListener {
+                override fun onDataChange(snapshot: DataSnapshot) {
+                    snapshot.getValue(clazz)?.let { trySend(it) }
+                        ?: throw IllegalStateException("Data is null")
+                }
+                
+                override fun onCancelled(error: DatabaseError) {
+                    throw IllegalStateException(error.message)
+                }
+            }
+            val ref = makeReference(*children)
+            ref.addValueEventListener(listener)
+            awaitClose { ref.removeEventListener(listener) }
+        }
+    
+    override fun <T : Any> subscribeToListOf(clazz: Class<T>, vararg children: String) =
+        callbackFlow<List<T>> {
+            val listener = object : ValueEventListener {
+                override fun onDataChange(snapshot: DataSnapshot) {
+                    val list = mutableListOf<T>()
+                    for (child in snapshot.children) {
+                        child.getValue(clazz)?.let { list.add(it) }
+                    }
+                    trySend(list)
+                }
+                
+                override fun onCancelled(error: DatabaseError) {
+                    throw IllegalStateException(error.message)
+                }
+            }
+            val ref = makeReference(*children)
+            ref.addValueEventListener(listener)
+            awaitClose { ref.removeEventListener(listener) }
+        }
+    
+    private fun makeReference(vararg children: String): DatabaseReference {
+        var ref = reference
+        children.forEach { ref = ref.child(it) }
+        return ref
+    }
+}
